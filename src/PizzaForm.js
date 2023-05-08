a295a6350210d21b89b6af94d0afc5079a1f3411
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-05-08 08:11:26 -0400

    need to pass 3 more

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -28,21 +28,51 @@ function PizzaForm() {
     };
   
     const handleSubmit = (event) => {
-      event.preventDefault();
-      const order = {
-        name,
-        size,
-        toppings,
-        specialInstructions,
+        event.preventDefault();
+      
+        // check if name is at least 2 characters long
+        if (name.length < 2) {
+          // display error message
+          const errorMessage = document.querySelector('#name-error-message');
+          errorMessage.style.display = 'block';
+          return;
+        }
+      
+        const order = {
+          name,
+          size,
+          toppings,
+          specialInstructions,
+        };
+      
+        console.log(order);
+        fetch('https://reqres.in/api/orders', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: JSON.stringify(order),
+        })
+          .then((response) => response.json())
+          .then((data) => {
+            console.log('Success:', data);
+            alert('Your order has been submitted successfully!');
+          })
+          .catch((error) => {
+            console.error('Error:', error);
+            alert('There was an error submitting your order. Please try again.');
+          });
       };
-      console.log(order);
-      // Submit the order to the API here
-    };
+      
+      
+      
   
     return (
       <form id="pizza-form" onSubmit={handleSubmit}>
         <label htmlFor="name-input">Name:</label>
-        <input type="text" id="name-input" value={name} onChange={handleNameChange} required minLength="2" />
+<input type="text" id="name-input" value={name} onChange={handleNameChange} minLength="2" required />
+{ name.length < 2 && <p className="error-message">name must be at least 2 characters</p> }
+
   
         <label htmlFor="size-dropdown">Size:</label>
         <select id="size-dropdown" value={size} onChange={handleSizeChange}>
