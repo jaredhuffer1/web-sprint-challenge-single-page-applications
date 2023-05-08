a48f40f73ae9c4f072232976cc3c7589d797c94d
jaredhuffer1 <jaredhuffer1@gmail.com>
2023-05-08 07:38:17 -0400

    got some test passing

--- src/PizzaForm.js
+++ src/PizzaForm.js
@@ -1,68 +1,81 @@
 import React, { useState } from 'react';
 
 function PizzaForm() {
-    const [form, setForm] = useState({
-        name: '',
-        size: 'Small',
-        toppings: {
-            Pepperoni: false,
-            Mushroom: false,
-            Onion: false,
-            Sausage: false,
-            blackOlives: false,
-
-
-        },
-        special: ''
-    });
-
+    const [name, setName] = useState('');
+    const [size, setSize] = useState('Small');
+    const [toppings, setToppings] = useState([]);
+    const [specialInstructions, setSpecialInstructions] = useState('');
+  
+    const handleNameChange = (event) => {
+      setName(event.target.value);
+    };
+  
+    const handleSizeChange = (event) => {
+      setSize(event.target.value);
+    };
+  
+    const handleToppingsChange = (event) => {
+      const { value, checked } = event.target;
+      if (checked) {
+        setToppings([...toppings, value]);
+      } else {
+        setToppings(toppings.filter((topping) => topping !== value));
+      }
+    };
+  
+    const handleSpecialInstructionsChange = (event) => {
+      setSpecialInstructions(event.target.value);
+    };
+  
+    const handleSubmit = (event) => {
+      event.preventDefault();
+      const order = {
+        name,
+        size,
+        toppings,
+        specialInstructions,
+      };
+      console.log(order);
+      // Submit the order to the API here
+    };
+  
     return (
+      <form id="pizza-form" onSubmit={handleSubmit}>
+        <label htmlFor="name-input">Name:</label>
+        <input type="text" id="name-input" value={name} onChange={handleNameChange} required minLength="2" />
+  
+        <label htmlFor="size-dropdown">Size:</label>
+        <select id="size-dropdown" value={size} onChange={handleSizeChange}>
+          <option value="Small">Small</option>
+          <option value="Medium">Medium</option>
+          <option value="Large">Large</option>
+        </select>
+  
+        <label>Toppings:</label>
         <div>
-            <form>
-                <label>
-                    Name:
-                    <input type="text" value={form.name} onChange={event => setForm({ ...form, name: event.target.value })} />
-                </label>
-                <br />
-                <label>
-                    Size:
-                    <select value={form.size} onChange={event => setForm({ ...form, size: event.target.value })}>
-                        <option value="Small">Small</option>
-                        <option value="Medium">Medium</option>
-                        <option value="Large">Large</option>
-                        <option value="NewYorkStyle">New York Style</option>
-                    </select>
-                </label>
-                <br />
-                <label>
-                    Toppings:
-                    <br />
-                    <input type="checkbox" checked={form.toppings.Pepperoni} onChange={() => setForm({ ...form, toppings: { ...form.toppings, Pepperoni: !form.toppings.Pepperoni } })} />
-                    <span>Pepperoni</span>
-                    <br />
-                    <input type="checkbox" checked={form.toppings.Mushroom} onChange={() => setForm({ ...form, toppings: { ...form.toppings, Mushroom: !form.toppings.Mushroom } })} />
-                    <span>Mushroom</span>
-                    <br />
-                    <input type="checkbox" checked={form.toppings.Onion} onChange={() => setForm({ ...form, toppings: { ...form.toppings, Onion: !form.toppings.Onion } })} />
-                    <span>Onion</span>
-                    <br />
-                    <input type="checkbox" checked={form.toppings.Sausage} onChange={() => setForm({ ...form, toppings: { ...form.toppings, Sausage: !form.toppings.Sausage } })} />
-                    <span>Sausage</span>
-                    <br/>
-                    <input type="checkbox" checked={form.toppings.Pepperoni} onChange={() => setForm({ ...form, toppings: { ...form.toppings, Pepperoni: !form.toppings.Pepperoni } })} />
-                    <span></span>
-                    <br/>
-                    <input type="checkbox" checked={form.toppings.blackOlives} onChange={() => setForm({ ...form, toppings: { ...form.toppings, blackOlives: !form.toppings.blackOlives } })} />
-                    <span>Black Olives </span>
-                </label>
-                <br />
-                <label>
-                    Special Instructions:
-                    <textarea value={form.special} onChange={event => setForm({ ...form, special: event.target.value })} />
-                </label>
-            </form>
+          <input type="checkbox" id="pepperoni" value="Pepperoni" onChange={handleToppingsChange} />
+          <label htmlFor="pepperoni">Pepperoni</label>
         </div>
+        <div>
+          <input type="checkbox" id="mushroom" value="Mushroom" onChange={handleToppingsChange} />
+          <label htmlFor="mushroom">Mushroom</label>
+        </div>
+        <div>
+          <input type="checkbox" id="sausage" value="Sausage" onChange={handleToppingsChange} />
+          <label htmlFor="sausage">Sausage</label>
+        </div>
+        <div>
+          <input type="checkbox" id="olives" value="Olives" onChange={handleToppingsChange} />
+          <label htmlFor="olives">Olives</label>
+        </div>
+  
+        <label htmlFor="special-text">Special Instructions:</label>
+        <textarea id="special-text" value={specialInstructions} onChange={handleSpecialInstructionsChange} />
+  
+        <button type="submit">Submit Order</button>
+      </form>
     );
-}
-
-export default PizzaForm;
+  }
+  
+  export default PizzaForm;
+  
